5e186ba917199559ee1c7869673fe0077163e7ad
liyi <373422363@qq.com>
2021-01-30 02:14:40 +0800

    fix: hide route

--- config/routes.ts
+++ config/routes.ts
@@ -20,12 +20,12 @@
   //     },
   //   ],
   // },
-  {
-    name: 'list.table-list',
-    icon: 'table',
-    path: '/list',
-    component: './TableList',
-  },
+  // {
+  //   name: 'list.table-list',
+  //   icon: 'table',
+  //   path: '/list',
+  //   component: './TableList',
+  // },
   {
     name: 'Dashboard',
     icon: '',
